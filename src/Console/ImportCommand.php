b579986683db56aaeca12625873221c961320d8f
charliekassel <ck@charliekassel.com>
2018-02-27 12:05:12 +0000

    Import command  (#12)
    * clear the entity manager while doing batch imports
    
    * add model count to command output
    
    * forget processed events

--- src/Console/ImportCommand.php
+++ src/Console/ImportCommand.php
@@ -47,10 +47,17 @@ class ImportCommand extends Command
         }
 
         $events->listen(ModelsImported::class, function ($event) use ($class) {
-            $this->line('<comment>Imported [' . $class . '] models up to ID:</comment> ' . $event->models->last()->getKey());
+            $this->line(sprintf(
+                '<comment>Imported %d [%s] models up to ID: %s</comment>',
+                $event->models->count(),
+                $class,
+                $event->models->last()->getKey()
+            ));
         });
 
         $repository->makeAllSearchable();
+
+        $events->forget(ModelsImported::class);
 
         $this->info('All [' . $class . '] records have been imported.');
     }
